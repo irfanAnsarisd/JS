9742c17d4135c4932991dcbaeb14e8fc67341fb7
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-09 18:35:53 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -13,7 +13,7 @@
  const id = Symbol('123')
  const anotherId = Symbol('123')
  
- console.log(id === anotherId);
+ //console.log(id === anotherId);
 
  const bigNumber = 3492857990703240347032403n
 
@@ -36,4 +36,31 @@
     console.log("hello world");
  }
 
- console.log(typeof bigNumber)
+ //console.log(typeof bigNumber)
+
+
+ // ++++++++++++++++++++++++++++++++++++++++++++
+
+ // stack (Primitive)=> copy banti hai,
+//   Heap(Non - primitive) => original value ka reference bnta hai
+
+
+ let myYoutubeName = "irfanOfficial"
+
+ let anotherName = myYoutubeName
+   anotherName = "chaiorcode"
+
+ console.log(anotherName);
+ console.log(myYoutubeName)
+
+ let userOne = {
+   email: "user@google.com",
+   upi: "user@ybl"
+ }
+
+ let userTwo = userOne
+
+ userTwo.email = "irfan@google.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
