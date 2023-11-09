309b4b2a89fe450da6e3b071ca82e04dd372db8c
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-09 06:34:22 +0000

    conversion, operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "irfan"
 
 let booleanIsLoogedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoogedIn);
+// console.log(booleanIsLoogedIn);
 
 /*
     1 => true
@@ -35,5 +35,48 @@ console.log(booleanIsLoogedIn);
 let someNuber = 33;
 
 let stringNumber = String(someNuber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+//  ***********************************OPERATIONS*****************************************
+
+ let value = 3;
+ let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "hello"
+let str2 = " Irfan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4)* 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 =  2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// prefix to postfix mdn
