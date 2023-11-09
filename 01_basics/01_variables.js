c204fc904f3d4263ec4f88591048ae09fd0fa625
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-09 05:50:44 +0000

    variables, datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 147738
+let accountEmail = "irfanansarisd648@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  // Not allowed 
+ console.log(accountId);
+
+ /*
+   prefer to not use  var
+   because of issue of block scope and functional scope.
+ */
+
+ accountEmail = "irfan@gmail.com"
+ accountPassword = "7383"
+ accountCity = "Delhi"
+
+ console.table([accountId,accountEmail,accountPassword, accountCity])
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // Treat all JS code as a newer version
+
+// alert(3 + 3) // we all are using nodejs, not browser
+
+console.log(3 + 4)
+console.log("hello")
+
+let name = "irfan"
+let age  = 22
+let isLoggedIn = false
+let state;
+// number => 2 to power 53
+// bigint
+// String => ""
+// null => standalone value
+// undefined => 
+// symbol =>  unique
+
+// object
+
+console.log(typeof "irfan");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined 
