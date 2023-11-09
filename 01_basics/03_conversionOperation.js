f5114cfd192187680b04a3a354a4aebadb87a0af
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-09 06:13:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = 33
+// let score = "33abc"
+let score = "33"
+// let score = null
+// let score = undefined
+
+// console.log(typeof score)
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// note
+/*
+    "33" => 33
+    "33abc" => NaN
+    true =>1 , false => 0
+*/
+
+// let isLoggedIn = ""
+// let isLoggedIn = 1
+let isLoggedIn = "irfan"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+/*
+    1 => true
+    0 => false
+    "" => false
+    "irfan" => true
+*/
