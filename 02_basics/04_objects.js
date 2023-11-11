bd2a65563515caa0c175e23faf3da7d21f038482
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-11 19:09:33 +0000

    objects and json api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -53,14 +53,31 @@ const user = [
 ]
 
 user[1].email
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    cousername: "js in hindi",
+    price: "999",
+    courseInstructor: "irfan"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
 
 
-
+// JSON format
+// {
+//     "Name": "irfan",
+//     "course": "js in hindi",
+//     "price": "990" 
+// }
  
