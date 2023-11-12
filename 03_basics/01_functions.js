6b5eabace37606616d55d404970643b94e4e7fe8
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-12 17:38:02 +0000

    function in JSSS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -64,6 +64,41 @@ function loginUserMessage(username = "sam") {
 }
 
 
-console.log(loginUserMessage("Irfan"))
+// console.log(loginUserMessage("Irfan"))
 // console.log(loginUserMessage(""))
 // console.log(loginUserMessage())
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function calculateCartPrice(...numb1) {
+    return numb1
+}   
+
+console.log(calculateCartPrice(200, 400, 600))
+console.log(calculateCartPrice(200, 400, 600, 3000))
+
+const user = {
+    username: "irfan",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 300, 400, 100]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,600]))
