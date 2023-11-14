7a16f712655360831cf7a111f87aecd6fd037fa0
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2023-11-14 18:00:36 +0000

    foreach , filter, map, reduce

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -5,5 +5,36 @@ coding.forEach(function(val) {
 });
 
 coding.forEach( (item) => {
-    console.log(item);
+    // console.log(item);
+})
+
+
+function printMe(item){
+    // console.log(item);
+}
+
+// coding.forEach(printMe)
+
+coding.forEach(  (item, index, arr) => {
+    // console.log(item, index, arr);
+} )
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languagefileName: "js"
+    },
+    {
+        languageName : "javascript",
+        languagefileName: "js"
+    },
+    {
+        languageName : "javascript",
+        languagefileName: "js"
+    },
+]
+
+myCoding.forEach( (item) =>{
+    // console.log(item.languageName);
+    // console.log(item.languagefileName);
 })
